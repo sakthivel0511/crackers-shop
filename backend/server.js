e19c7b7ce7ef61fda6b3cbb8d0bf96158a6ba00a
muthukumar-29 <muthukumarm.2903@gmail.com>
2025-11-01 17:11:28 +0530

    Updated MongoDB connection and Twilio config in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 const sendOrderRoute = require('./routes/sendmsg');
 
-
+import dotenv from "dotenv";
+dotenv.config();
 
 
 const app = express();
@@ -14,8 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-dbURI = 'mongodb://localhost:27017/crackers';
-mongoose.connect(dbURI, {
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
